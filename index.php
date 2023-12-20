146d6d67e7913da84b1d616ed2013eb366ac4c7e
LunarCreature <luixlt@yahoo.com>
2023-12-20 18:14:59 +0200

    fixed a bug

--- index.php
+++ index.php
@@ -22,12 +22,13 @@
     <main>
         <form method="get">
             <input type="text" name="input">
+            <input type="text" name="symbol">
             <button type="submit">Submit</button>
         </form>
         <?php
             class ExerciseClass
             {
-                public static function countTheLetters(string $input): string
+                public static function countTheLetters(string $input, string $counter): string
                 {
                     $letters = preg_replace("/[^A-Za-z]/", '', $input); // delete non-letters
                     $letters = strtolower($letters); // get rid of capitalisation
@@ -36,13 +37,15 @@
                     
                     $uniqueLetters = array_unique($letters); //make a unique letter list
                     $letterCounts = array_flip($uniqueLetters); // make a counter list
-                    for ($i = 0; $i < count($uniqueLetters); $i++) // set all counters to 0
-                        $letterCounts[$i] = '';
+                    // for ($i = 0; $i < count($uniqueLetters); $i++) // bug. Keys aren't numbers in this array
+                    //     $letterCounts[$i] = '';
+                    foreach ($uniqueLetters as $letter)  // set all counters to 0
+                        $letterCounts[$letter] = '';
 
                     foreach ($letters as $letter) //count each letter
                     {
                         if (in_array($letter, $uniqueLetters))
-                            $letterCounts[$letter] .= '*';
+                            $letterCounts[$letter] .= $counter;
                     }
 
                     //Output
@@ -57,9 +60,12 @@
 
             
             $input = $_GET['input'] ?? ''; //check if form is submitted
+            $counter = $_GET['symbol'] ?? ''; //check if form is submitted
 
             if($input)
-                echo('<br><br>'. ExerciseClass::countTheLetters($input));
+            {
+                echo('<br><br>'. ExerciseClass::countTheLetters($input, $counter));
+            }
         ?>
     </main>
 </body>
